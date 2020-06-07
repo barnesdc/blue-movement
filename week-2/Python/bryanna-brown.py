8ad48489b73a6120d4edf8cbabcf907ef5b8a346
Bryanna <bbrown001129@gmail.com>
2020-06-07 17:32:47 -0400

    Added function to find longest palindrome... need to debug both of my functions

--- week-2/Python/bryanna-brown.py
+++ week-2/Python/bryanna-brown.py
@@ -1,6 +1,10 @@
 
 # Find Palindrome finds
 
+"""
+This function returns true if the given string
+is a palindrome and false other wise
+"""
 def findPalindrome(string):
     # even or odd number of characters?
     strLength = len(string)
@@ -9,14 +13,27 @@ def findPalindrome(string):
     else:
         strLength = int(strLength // 2) - 1
     # Reverse String half   
-    flipString = string[strLength:0:-1] + string[0]
-
-    if flipString in string:
+    flipString = string[strLength:0:-1] + string[0] 
+    if flipString == string[0:strLength+1]:
         return True
     else:
         return False
 
+def longestPalindrome(inputStr):
+    # Check full string
+    if findPalindrome(inputStr):
+        print(findPalindrome)
+        return inputStr
+    else:
+        subStrLength = len(inputStr) - 1
+        for i in range(subStrLength,2,-1):
+            for j in range(0,i):
+                end = j + i
+                if findPalindrome(inputStr[j:end]):
+                    return inputStr[j:end]
+        return "no Palindromes"
 
-
-out = findPalindrome("ababa")
-print(out)
+out1 = findPalindrome("abadad")
+out2 = longestPalindrome("asabad")
+print(out1)
+print(out2)    
