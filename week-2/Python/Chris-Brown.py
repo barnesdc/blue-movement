beeb20111ae8dc4fa2894370a64e838b60396827
Chris Brown <50889776+CodingWithChrisB@users.noreply.github.com>
2020-06-08 12:50:24 -0400

    Fixed bugs in code for week 2 prompt

--- week-2/Python/Chris-Brown.py
+++ week-2/Python/Chris-Brown.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python3
+
 from itertools import permutations
+import argparse
+
 
 #Define a funtion isPalendrome() to do a check on a string
 def isPalindrome(s):
@@ -9,6 +13,9 @@ def isPalindrome(s):
     if reverse == s:
         return True
 
+    else:
+        return False
+
 #Now use isPalindrome() to check a substring in this function
 def longestPal(s):
 
@@ -18,9 +25,9 @@ def longestPal(s):
     for ch in s:
 
         #add one character at a time to sub then check
-        sub += s
+        sub += ch
 
-        if isPalindrome(sub) == True:
+        if isPalindrome(sub):
             longest = sub
 
     return longest
@@ -33,7 +40,32 @@ def longestPal(s):
 
 def printPermutations(s):
 
-    perm = Permutations(s)
+    perm = permutations(s)
+    result_list = []
 
     for i in perm:
-        print(i)
+        x = ''.join(i)
+        result_list.append(x)
+
+    print(' '.join(result_list))
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser(description='From File -F or Text input -T')
+    ap.add_argument("-F", "--file", required=False, type=str, help='filename')
+    ap.add_argument("-T", "--text", required=False, type=str, help='text input')
+    args = ap.parse_args()
+    if args.file:
+        palindrom = open(args.file, 'r')
+        for line in palindrom:
+            print(isPalindrome(args.file))
+            print(longestPal(args.file))
+            printPermutations(args.text)
+    elif args.text:
+        print(isPalindrome(args.text))
+        print(longestPal(args.text))
+        print()
+        printPermutations(args.text)
+    else:
+        print("ERROR: No Input")
+        exit(1)
