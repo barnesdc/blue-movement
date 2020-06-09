3ff51d58144d86e3956698a98c28e5b9846199c8
Emmanuel Noufele <noufelee@yahoo.com>
2020-06-08 21:56:57 -0500

    week2 prompt problem 1
    wrote a program that given a string, it return the longest substring , which is palindrome

---
+++ week-2/Java/Emmanuel-Noufele.java
@@ -0,0 +1,50 @@
+public class longestSubstringPalindrome {
+	
+	public static String findLongestSubstring(String str) {
+		String palindrome = "";
+		int maxLength = 1;
+		int begin = 0;
+		int low;
+		int high;
+		
+		for(int i = 0; i < str.length(); i++) {	
+			// even case
+			low = i;
+			high = i + 1;
+			while(low >= 0 && high < str.length() && str.charAt(low) == str.charAt(high)) {
+				if(high - low + 1 > maxLength) {
+					begin = low;
+					maxLength = high - low + 1;
+				}
+				low--;
+				high++;
+			}
+			
+			// odd case
+			low = i;
+			high = i + 2;
+			while(low >= 0 && high < str.length() && str.charAt(low) == str.charAt(high)) {
+				if(high - low + 1 > maxLength) {
+					begin = low;
+					maxLength = high - low + 1;
+				}
+				low--;
+				high++;
+			}
+		}
+		palindrome = str.substring(begin, begin + maxLength);
+		return palindrome;
+	}
+
+	public static void main(String[] args) {
+		String input = "nhgdpsjdnfbeeebbjsjsb";
+		String result = "";
+		if(input.isEmpty() || input.length() == 1) {
+			System.out.println(input);
+		} else {
+			 result = findLongestSubstring(input);
+			
+		}
+		System.out.println(result);	
+	}
+}
