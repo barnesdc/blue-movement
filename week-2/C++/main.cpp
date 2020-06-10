0e99b907bb5bfcfcb3026de8c145cfa5abd5bb19
Omari Pau <OVP@Omaris-MacBook-Pro-2.local>
2020-06-09 19:24:30 -0500

    1st commit

--- week-2/C++/main.cpp
+++ week-2/C++/main.cpp
@@ -1,2 +1,28 @@
 #include <iostream>
-#include <algorithm>
+
+using namespace std;
+
+void palindrome()
+{
+    char str[100] = "madam";
+    int length = strlen(str);
+    int flag = true;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (str[i] != str[length - 1 - i])
+        {
+            flag = false;
+            break;
+        }
+    }
+    if (flag == true)
+        cout << "The word " << str << " is palindrome!";
+    else
+        cout << "The word " << str << " is not palindrome!";
+}
+
+int main()
+{
+    palindrome();
+}
