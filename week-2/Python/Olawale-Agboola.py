c13c831ee7d284eecd61c38b05b8a201b39cd0cb
lexromeo <66388802+lexromeo@users.noreply.github.com>
2020-06-10 15:47:17 -0500

    Update Olawale-Agboola.py
    clean up code

--- week-2/Python/Olawale-Agboola.py
+++ week-2/Python/Olawale-Agboola.py
@@ -4,7 +4,7 @@ def longestPalindromicSubstring(s):
         dp[i][i] = True
     max_length = 1
     start = 0
-    for l in range(2,len(s)+1):
+    for l in range(2, len(s) + 1):
         for i in range(len(s) - l + 1):
             end = i + l
             if l == 2:
@@ -17,17 +17,20 @@ def longestPalindromicSubstring(s):
                   dp[i][end - 1] = True
                   max_length = l
                   start = i
-      return s[start:start+max_length]
+    return s[start:start + max_length]
 
 print(longestPalindromicSubstring("ababa"))
 
+
 def permutations(string, step = 0):
     if step == len(string):
-        print "".join(string)
+        print("".join(string))
 
-     for i in range(step, len(string)):
+    for i in range(step, len(string)):
         string_copy = [c for c in string]
-        string_copy[step], string_copy[i] =string_copy[i], string_copy[step]
+        string_copy[step], string_copy[i] = string_copy[i], string_copy[step]
         permutations(string_copy, step + 1)
 
 print(permutations('abb'))
+
+#C:\Users\olawa\AppData\Local\Programs\Python\Python38-32
