db5e38498490efc95ee36193811873b6667dfcba
Dee Berry <deiondreaberry@gmail.com>
2020-06-09 23:29:33 -0400

    Cleaned up and Commented

--- week-2/C++/main.cpp
+++ week-2/C++/main.cpp
@@ -1,42 +1,37 @@
 #include <iostream>
-#include <vector>
-#include <iomanip>
-#include <iterator>
 #include <algorithm>
 using namespace std;
 
-int fact(int n)
-{
+//factorial
+int fact(int n){
   if(n < 1) return 1;
-  else
-      return n* fact(n-1);
+  else return n* fact(n-1);
 }
 
+//check if string is palindrome
 bool isPalindrome(string str){
   int length = str.length();
+
   for(int i = 0; i< length; i++)
-  {
     if(str[i] != str[length - i - 1])
       return false;
-  }
+
   return true;
 }
+
+//find longest substring and return it
 string longest_substring(string str){
-  int len = str.length(); //13
-  int first, len_of_substr = len;
+  int first,
+      len = str.length(),
+      len_of_substr = len;
   bool yes = false;
-  string hold = "";
-  string s = "";
+  string hold = "", s = "";
 
   for (int i = 1; i <= len; i++)
   {
     first = 0;
     for(int j = 0; j < i; j++)
     {
-      /*
-      if(last < 10) cout<<' ';
-      cout<<first<<" - "<<last<< "   ";
-      */
       s = str.substr(first, len_of_substr);
       if(isPalindrome(s))
       {
@@ -46,27 +41,25 @@ string longest_substring(string str){
       first++;
     }
     len_of_substr--;
+    //if found a palindrome substring return it
     if(yes) return hold;
   }
 
   return "";
 }
+
+//find and print all permutations of string
 void permutations(string s){
-  //vector<char> each_perm = s;
   int num = fact(s.length());
 
   for(int i = 0; i< num; i++)
   {
     next_permutation(s.begin(),s.end());
-
-    cout<<setw(4)<<i + 1<< ": "<< s << endl;
-
+    cout<<i + 1<< ": "<< s << endl;
   }
-
 }
-int main(){
-  //cout << (isPalindrome("alab")? "true" : "false");
 
+int main(){
   string holder, str;
   str = "bababa";
   holder = longest_substring(str);
@@ -78,8 +71,3 @@ int main(){
 
   return 0;
 }
-
-/*Method: Find every possible substring
-from biggest to smallest. CHeck if a palindrome,
-add it to returning string.
-*/
