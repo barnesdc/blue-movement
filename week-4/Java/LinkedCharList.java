2a4fa493b3caf2b1479bcbbde88b442de3158bb9
Johanne Antoine <joantoine9@gmail.com>
2020-06-24 17:17:14 -0400

    added two Linked List files, one for integers and one for characters

---
+++ week-4/Java/LinkedCharList.java
@@ -0,0 +1,81 @@
+class LinkedCharList {
+    Node head;
+    static class Node {
+        char data;
+        Node next;
+        Node prev;
+        Node (char d) {
+            data = d;
+            next = null;
+            prev = null;
+        }
+    }
+    public void insert(Node n) {
+        if (this.head == null) {
+            this.head = n;
+        } else {  
+             Node pointer = this.head;
+            while (pointer.next != null) {
+                pointer = pointer.next;
+            }
+            pointer.next = n;
+            n.prev = pointer;
+        }
+    }
+    LinkedCharList(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            this.insert(new Node(s.charAt(i)));
+        }
+    }
+
+    public static void printList(Node head) {
+        Node pointer = head;
+        while (pointer != null) {
+            System.out.print(pointer.data);
+            pointer = pointer.next;
+        }
+    }
+
+    public Node reverse(Node n) {
+        Node prev = null; 
+        Node current = n; 
+        Node next = null; 
+        while (current != null) { 
+            next = current.next; 
+            current.next = prev; 
+            prev = current; 
+            current = next; 
+        } 
+        n = prev; 
+        return n; 
+    }
+
+    public static Node reverseSentence(LinkedCharList cl) {
+        Node n = cl.reverse(cl.head);
+        LinkedCharList result = new LinkedCharList("");
+        LinkedCharList temp = new LinkedCharList("");
+        Node p = n;
+        while (p != null) {
+            if (p.data == ' ') {
+                temp.head = temp.reverse(temp.head);
+                result.insert(temp.head);
+                result.insert(new Node(p.data));
+                temp = new LinkedCharList("");
+            } 
+            else {
+                temp.insert(new Node(p.data));
+                if (p.next == null) {
+                    temp.head = temp.reverse(temp.head);
+                    result.insert(temp.head);
+                }
+            }
+            p = p.next;
+        }
+        return result.head;
+    }
+    public static void main(String[] args) {
+        LinkedCharList charList = new LinkedCharList("Geeks for Geeks I love");
+        //printList(charList.reverse(charList.head));
+        printList(reverseSentence(charList));
+    }
+}
