94f725d62f1e8ea1b12c4b73ceecee13ec59399f
Chord28EE <66052218+Chord28EE@users.noreply.github.com>
2020-06-24 16:33:52 -0700

    test

--- week-4/Java/cordero-woods.java
+++ week-4/Java/cordero-woods.java
@@ -1,22 +1,65 @@
 class LinkedList { 
   
-    static Node head; 
+    //static Node head; 
   
     static class Node { 
   
-        int data; 
+        char data; 
         Node next; 
   
-        Node(int d) 
+        Node(char d) 
         { 
             data = d; 
             next = null; 
         } 
     } 
   
+    public static Node reverseLL(Node a){
+
+        StringBuilder temp = new StringBuilder();
+
+        while(a.next != null){
+            temp.append(a.data);
+        }
+
+        temp.reverse();
+        String s = temp.toString();
+        Node t = new Node ();
+        Node head = t;
+
+        for(int i = 0;i<s.length();i++){
+            t.data = s.charAt(i);
+            t = t.next();
+        }
+
+        return head;
+
+    }
+
+    public static Node createList(String s){
+        Node list =  new Node();
+        Node list2 = list;
+        for (int i = 0;i<s.length();i++){
+            list.data = s.charAt(i);
+            list = list.next;
+        }
+        return list2;
+    }
+
+    public static printList(Node a){
+        while(a.data){
+            System.out.println(a.data);
+            a = a.next;
+        }
+    }
     public static void main(String[] args) 
     { 
-        System.out.println("test");
+        String s = "hello";
+        //System.out.println("hi");
+        Node list = createList(s);
+        list = reverseLL(list);
+        printList(list);      
+
       
     } 
 } 
