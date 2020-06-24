8200dc67df2fec5f6d44b0c64adb3e06590e3874
Chord28EE <66052218+Chord28EE@users.noreply.github.com>
2020-06-24 15:28:04 -0700

    Test

---
+++ week-4/Java/cordero.java
@@ -0,0 +1,21 @@
+class cordero { 
+  
+    static Node head; 
+  
+    static class Node { 
+  
+        int data; 
+        Node next; 
+  
+        Node(int d) 
+        { 
+            data = d; 
+            next = null; 
+        } 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+      
+    } 
+} 
