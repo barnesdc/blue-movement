d3710e0d6b7e13f70174fa1e437a702009441202
Chord28EE <66052218+Chord28EE@users.noreply.github.com>
2020-06-24 17:00:52 -0700

    update

--- week-4/Java/cordero.java
+++ week-4/Java/cordero.java
@@ -1,7 +1,5 @@
 public class cordero { 
-  
-    //static Node head; 
-  
+  //null pointer exception, not sure how to fix need more time to troubleshoot
     static class Node { 
   
         char data; 
@@ -24,12 +22,12 @@ public class cordero {
 
         temp.reverse();
         String s = temp.toString();
-        Node t = new Node ();
+        Node t = new Node ('c');
         Node head = t;
 
         for(int i = 0;i<s.length();i++){
             t.data = s.charAt(i);
-            t = t.next();
+            t = t.next;
         }
 
         return head;
@@ -37,7 +35,7 @@ public class cordero {
     }
 
     public static Node createList(String s){
-        Node list =  new Node();
+        Node list = new Node('e');
         Node list2 = list;
         for (int i = 0;i<s.length();i++){
             list.data = s.charAt(i);
@@ -46,8 +44,8 @@ public class cordero {
         return list2;
     }
 
-    public static printList(Node a){
-        while(a.data){
+    public static void printList(Node a){
+        while(a.next != null){
             System.out.println(a.data);
             a = a.next;
         }
@@ -55,7 +53,6 @@ public class cordero {
     public static void main(String[] args) 
     { 
         String s = "hello";
-        //System.out.println("hi");
         Node list = createList(s);
         list = reverseLL(list);
         printList(list);      
