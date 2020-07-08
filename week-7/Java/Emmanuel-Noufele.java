d39f1bd5a0f4c2a9c4d1131319755a0f038ce362
Emmanuel Noufele <noufelee@yahoo.com>
2020-07-08 01:14:33 +0100

    Solution for the second prompt

--- week-7/Java/Emmanuel-Noufele.java
+++ week-7/Java/Emmanuel-Noufele.java
@@ -37,3 +37,67 @@ public class maximumDifference {
 		System.out.println("The maximum difference between a node and it's descendant is " + maxDiff);
 	}
 }
+
+
+package hackerrsnk;
+
+import java.util.Stack;
+
+public class printAllLeafToRoot {
+	
+	public static boolean isLeaf(TreeNode node) {
+		
+		// Check if the right child and the left child are both null
+		if(node.left == null && node.right == null) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static void printCurrentPath(Stack <Integer> stack) {
+		
+		for(int i = stack.size() - 1; i >= 0; i--) {
+			System.out.print(stack.get(i));
+			if(i > 0) {
+				System.out.print(" -> ");
+			}
+		}
+		System.out.println();
+	}
+	
+	public static void printAllPaths(TreeNode node, Stack<Integer> stack) {
+		
+		if(node == null) {
+			return;
+		}
+		
+		// add the current node to the stack
+		stack.add(node.val);
+		
+		// Check if the current node is a leaf node
+		if(isLeaf(node)) {
+			printCurrentPath(stack);
+		}
+		
+		// recurse for left and right subtree
+		printAllPaths(node.left, stack);
+		printAllPaths(node.right, stack);
+		
+		// This will remove the current node after we are done with the left and right subtree
+		stack.pop();
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeNode root = new TreeNode(1, new TreeNode(2, new TreeNode(4, null, null), new TreeNode(5, null, null)), 
+				new TreeNode(3, new TreeNode(6, new TreeNode(8, null, null), new TreeNode(9, null, null)), new TreeNode(7, null, null)));
+		Stack <Integer> stack = new Stack<Integer>();
+		
+		// print all paths from leaf to root
+		printAllPaths(root, stack);
+		
+	}
+
+}
+
