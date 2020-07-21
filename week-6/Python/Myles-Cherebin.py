aca263b23cb15dcdd5dd6ce295a8744b67bca8b8
MylesCherebin <45438388+MylesCherebin@users.noreply.github.com>
2020-07-20 21:55:10 -0400

    Add files via upload

--- week-6/Python/Myles-Cherebin.py
+++ week-6/Python/Myles-Cherebin.py
@@ -4,84 +4,105 @@ class Node:
         self.right = None
         self.val = key
 
-    def print_all_paths(node, path):
-        if (node is None):
-            return
+    def print(self):
+        a = self.head
+        out = ""
+        while (a is not None):
+            out = out + str(a.data) + "->"
+            a = a.next
 
-        path.append(node.val)
+        out += "NULL"
 
-        if (node.left is None and node.right is None):
-            path.reverse()
-            print(path)
-            path.reverse()
-        else:
-            node.left.print_all_paths(path)
-            node.right.print_all_paths( path)
+        print(out)
 
-        path.pop()
+def print_all_paths(node, path):
+    if (node is None):
+        return
+    path.append(node.val)
+    if (node.left is None and node.right is None):
+        path.reverse()
+        print(path)
+        path.reverse()
+    else:
+        print_all_paths(node.left, path)
+        print_all_paths(node.right, path)
+    path.pop()
 
-    def print_all(node):
-        path = []
-
-        node.print_all_paths(path)
-
+def print_all(node):
+    path = []
+    print_all_paths(node,path)
 
 
-    def max_dif_help(Node,val, max):
-        if (Node.left is None and Node.right is None):
-            return
 
-        if (Node.left is not None):
-            if (val - Node.left.val > max):
-                max = val - Node.left.val
+def max_dif_help(Node, maxdif):
+  if (Node.left is None and Node.right is None):
+    return (maxdif - Node.val)
 
-        if (Node.right is not None):
-            if (val - Node.right.val > max):
-                max = val - Node.right.val
+  maxdif = max(maxdif, Node.val)
 
-        max_dif_help(Node.left, val, max)
+  if (Node.left is not None):
+    dif_left = max_dif_help(Node.left,maxdif)
+  else:
+    dif_left = 0
 
-    def max_dif(Node):
-        max = 0
-        val = Node.val
-        temp = Node.left
+  if (Node.right is not None):
+    dif_right = max_dif_help(Node.right,maxdif)
 
-        while (temp is not None):
-            if (val - temp.val > max):
-                max = val - temp.val
+  else:
+    dif_right = 0
+  return max(maxdif - Node.val, max(dif_left,dif_right))
 
-                temp = temp.left
-            else:
-                temp = temp.left
+def max_dif(Node):
+  if (Node.left is None and Node.right is None):
+    return Node.val
 
-        return(max)
+  maxdif = max_dif_help(Node, Node.val)
+  return(maxdif)
 
 
 # PROMPT 3 CONVERT A TREE TO ITS MIRROR
-    def mirror_swap(node):
-        if((node.left is not None) or (node.right is not None)):
-             temp = node.left
-             node.left = node.right
-             node.right = temp
-
-             node.left.mirror_swap()
-             node.right.mirror_swap()        
+def mirror_swap(node):
+    if((node.left is not None) or (node.right is not None)):
+         temp = node.left
+         node.left = node.right
+         node.right = temp
+         mirror_swap(node.left)
+         mirror_swap(node.right)
 
 
 if __name__ == "__main__":
- '''      10
-         /  \
-        6    7
-       / \  / \
-      2   3 4  5'''
-a = Node(10)
-b = Node(6)
-c = Node(7)
-b.left = Node(2)
-b.right = Node(3)
-c.left = Node(4)
-c.right = Node(5)
+    print("This is the tree")
+    print()
+    print("   10   ")
+    print("  /  \  ")
+    print(" 6    7 ")
+    print("/ \  / \\")
+    print("2 3  4   5")
 
-a.left = b
-a.right = c
-a.print_all()
+    a = Node(10)
+    b = Node(6)
+    c = Node(7)
+    b.left = Node(2)
+    b.right = Node(3)
+    c.left = Node(4)
+    c.right = Node(5)
+
+    a.left = b
+    a.right = c
+    print()
+    print("from left to right, the paths are:")
+    print_all(a)
+
+    print()
+    print()
+
+    mirror_swap(a)
+    print("after being mirrored, from left to right, the paths are:")
+    print_all(a)
+
+    print()
+    print()
+    
+    print("maximum difference of tree is " + str(max_dif(a)))
+    print("maximum difference of left subtree is " + str(max_dif(b)))
+    print("maximum difference of right subtree is " + str(max_dif(c)))
